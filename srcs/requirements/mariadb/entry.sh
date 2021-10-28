84465c3ff897d1bc7e1b743594a919a8d2a904b9
hheimerd <32-kot@mail.ru>
2021-10-28 16:21:07 +0300

    try to fix mariadb

--- srcs/requirements/mariadb/entry.sh
+++ srcs/requirements/mariadb/entry.sh
@@ -1,14 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
-/etc/init.d/mysql start
+# /usr/bin/mysql_install_db --datadir="/var/lib/mysql"
 
-echo "
-CREATE DATABASE $DB_NAME;
-CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+/etc/init.d/mariadb setup
+
+echo '/etc/init'
+service mysql start
+
+mysql -u root --skip-password -e  "CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
 GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' WITH GRANT OPTION;
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASSWORD');
-FLUSH PRIVILEGES;" | mysql -u root --skip-password
+FLUSH PRIVILEGES;"
+
+# /etc/init.d/mysql start
 
 
-/usr/bin/mysql_install_db --datadir="/var/lib/mysql"
-/usr/bin/mysqld_safe --datadir="/var/lib/mysql"
+# sudo mysql  < /init.sql 
+
+mysql -u root  < /tmp/init.sql
+
+mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+
+service mysql stop
+
+echo end
+# /usr/bin/mysql_secure_installation
